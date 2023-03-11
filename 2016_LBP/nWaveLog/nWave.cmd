238977a1cfe3471237a3955365d35bede20c6e93
lucynth <98674845+derek8955@users.noreply.github.com>
2023-03-11 15:37:29 +0800

    Delete 2016_LBP/nWaveLog directory

--- 2016_LBP/nWaveLog/nWave.cmd
+++
@@ -1,7 +0,0 @@
-wvResizeWindow -win $_nWave1 1912 1 1920 1017
-wvSetPosition -win $_nWave1 {("G1" 0)}
-wvOpenFile -win $_nWave1 {/home/t107360223/Desktop/ic_contest/2016_LBP/LBP.fsdb}
-wvGetSignalOpen -win $_nWave1
-wvGetSignalSetScope -win $_nWave1 "/testfixture"
-wvGetSignalSetScope -win $_nWave1 "/testfixture/LBP"
-wvExit
