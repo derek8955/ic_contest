8c296f596d3211045645317b3597e6f0ea709b6d
lucynth <98674845+derek8955@users.noreply.github.com>
2023-03-11 09:33:53 +0800

    Add files via upload

--- 2017_DT/DT.v
+++ 2017_DT/DT.v
@@ -1,214 +1,204 @@
 module DT(
 	input 			clk, 
 	input			reset,
-	output	reg		done ,
-	output	reg		sti_rd ,
-	output	reg 	[9:0]	sti_addr ,
+	output	reg		done , //##
+	output			sti_rd ,//
+	output	reg 	[9:0]	sti_addr ,//
 	input		[15:0]	sti_di,
-	output	reg		res_wr ,
-	output	reg		res_rd ,
-	output	reg 	[13:0]	res_addr ,
-	output	reg 	[7:0]	res_do,
-	input		[7:0]	res_di,
-	output reg fw_finish
-	);
-
-//================================================================
-//------------------------Self defined--------------------------
-//================================================================
-
-reg [3:0] state, nx_state;
-reg [3:0] counter;
-wire [13:0] res_addr_NE, res_addr_W, res_addr_origin, res_addr_E, res_addr_SW, res_addr_origin_back;
-reg [7:0] minimum;
-
+	output		res_wr ,//
+	output			res_rd ,
+	output	reg 	[13:0]	res_addr ,//
+	output	reg 	[7:0]	res_do,//
+	input		[7:0]	res_di
+);
 //================================================================
 //  integer / genvar / parameters
 //================================================================
-
-parameter IDLE = 4'd0, RD_INIT = 4'd1, WR_INIT = 4'd2, BEFORE_RD_FOR = 4'd3, RD_FOR = 4'd4, FORWARD = 4'd5, WR_FOR = 4'd6; 
-parameter FORWARD_FN = 4'd7, RD_BACK = 4'd8, BACKWARD = 4'd9 , WR_BACK = 4'd10, FN = 4'd11;
+integer i;
 
 //================================================================
-//---------------------------design-----------------------------
+//   Wires & Registers 
 //================================================================
+reg [15:0] idata_buf;
 
-//current state logic
-always@( posedge clk or negedge reset ) 
-begin
-	if( !reset ) state <= IDLE;
-	else state <= nx_state;
+reg [3:0] dup_cnt;
+reg [2:0] cnt_calc;
+reg flag_fb;
 
+reg [6:0] y, x ;
+
+reg [7:0] mini;
+reg [7:0] res_input_buf;
+//================================================================
+//  FSM
+//================================================================
+parameter STATE_IDLE = 3'd0, STATE_INPUT = 3'd1, STATE_CALC = 3'd2, STATE_OUTPUT = 3'd3, STATE_READ_MEM = 3'd4, STATE_DELAY =3'd5, STATE_DUPLICATE = 3'd6, STATE_FINISH = 3'd7;
+
+reg [2:0] cur_state, nx_state;
+
+always @( posedge clk or negedge reset ) begin
+	if( !reset ) cur_state <= STATE_IDLE;
+	else cur_state <= nx_state;
 end
 
-//next state logic
-always@( * )
-begin
-	case(state)
-	IDLE: nx_state = RD_INIT;
-	RD_INIT: nx_state = WR_INIT;
-	WR_INIT:
-	begin
-		if( counter == 4'd15 )
-		begin
-			if( res_addr == 14'd16383 ) nx_state = BEFORE_RD_FOR;
-			else nx_state = RD_INIT;
+always @( * ) begin
+	case( cur_state ) 
+		STATE_IDLE: nx_state = STATE_INPUT;
+		STATE_INPUT: nx_state = STATE_DUPLICATE;
+		STATE_DUPLICATE: begin
+			if( dup_cnt == 0 ) begin
+				if( sti_addr == 1023 ) nx_state = STATE_READ_MEM;
+				else nx_state = STATE_INPUT;
+			end
+			else nx_state = STATE_DUPLICATE;
 		end
-		else nx_state = WR_INIT;
-	end
-	BEFORE_RD_FOR: nx_state = RD_FOR; // delay one clk
-	RD_FOR: 
-	begin
-		if( res_di ) nx_state = FORWARD;
-		else nx_state = ( res_addr == 14'd16254 )? FORWARD_FN : RD_FOR;
-	end
-	FORWARD: nx_state = ( counter == 4'd5 ) ? WR_FOR : FORWARD ;
-	WR_FOR: nx_state = ( res_addr == 14'd16254 )? FORWARD_FN : RD_FOR;
-	FORWARD_FN: nx_state = RD_BACK;
-	RD_BACK:
-	begin
-		if( res_di ) nx_state = BACKWARD;
-		else nx_state = ( res_addr == 14'd129 )? FN : RD_BACK;
-	end
-	BACKWARD: nx_state = ( counter == 4'd6 ) ? WR_BACK : BACKWARD ;
-	WR_BACK: nx_state = ( res_addr == 14'd129 )? FN : RD_BACK;
-	FN: nx_state = FN;
-	default: nx_state = IDLE;
-	endcase
-
+		STATE_READ_MEM: begin
+			if ( res_addr == 0 && flag_fb ) nx_state = STATE_FINISH;
+			else if( res_addr == 16383 && !flag_fb ) nx_state = STATE_DELAY;
+			else nx_state = ( res_di != 0 )? STATE_CALC : STATE_READ_MEM;
+		end
+		STATE_CALC: nx_state = ( cnt_calc == 4 )? STATE_OUTPUT : STATE_CALC ;
+		STATE_OUTPUT: nx_state = STATE_READ_MEM;
+		STATE_DELAY: nx_state = STATE_READ_MEM;
+		STATE_FINISH: nx_state = STATE_FINISH;
+		default: nx_state = STATE_IDLE;
+	endcase	
 end
 
-//sti_addr
-always@( posedge clk or negedge reset )
-begin
-	if( !reset ) sti_addr <= 10'd0;
-	else if( state == RD_INIT ) sti_addr <= sti_addr + 10'd1;
-end
+//================================================================
+//  INPUT
+//================================================================
+assign sti_rd = ( cur_state == STATE_INPUT );
 
-//res_addr
-assign res_addr_NE = res_addr - 14'd126;
-assign res_addr_W = res_addr - 14'd1;
-assign res_addr_origin = res_addr + 14'd129;
-assign res_addr_E = res_addr + 14'd1;
-assign res_addr_SW = res_addr + 14'd126;
-assign res_addr_origin_back = res_addr - 14'd129;
-
-always@( posedge clk or negedge reset )
-begin
-	if( !reset ) res_addr <= 14'd0;
-	else if( state == WR_INIT ) res_addr <= res_addr + 14'd1;
-	else if( state == BEFORE_RD_FOR ) res_addr <= 14'd128;
-	else if( nx_state == RD_FOR ) res_addr <= res_addr + 14'd1;
-	else if( nx_state == RD_BACK ) res_addr <= res_addr - 14'd1;
-	else if( nx_state == FORWARD )
-	begin
-		case(counter)
-		4'd0: res_addr <= res_addr_W;
-		4'd1: res_addr <= res_addr_NE;
-		4'd2: res_addr <= res_addr_W;
-		4'd3: res_addr <= res_addr_W;
-		4'd4: res_addr <= res_addr_origin;
-		endcase	
-	end
-	else if( nx_state == BACKWARD )
-	begin
-		case(counter)
-		4'd0: res_addr <= res_addr;
-		4'd1: res_addr <= res_addr_E;
-		4'd2: res_addr <= res_addr_SW;
-		4'd3: res_addr <= res_addr_E;
-		4'd4: res_addr <= res_addr_E;
-		4'd5: res_addr <= res_addr_origin_back;
-		endcase	
+always @(posedge clk or negedge reset ) begin
+	if( !reset ) idata_buf <= 0;
+	else if( cur_state == STATE_INPUT ) begin
+		for( i=0; i<16; i=i+1 ) idata_buf[15-i] <= sti_di[i];
 	end
 end
 
-//res_do
-always@( posedge clk or negedge reset )
-begin
-	if( !reset ) res_do <= 8'd0;
-	else if( nx_state == WR_INIT ) res_do <= sti_di[counter];
-	else if( nx_state == WR_FOR ) res_do <= minimum + 8'd1;
-	else if( nx_state == WR_BACK ) res_do <= minimum;
+always @(posedge clk or negedge reset) begin
+	if(!reset) res_input_buf <= 0;
+	else if(cur_state == STATE_READ_MEM) res_input_buf <= res_di;
+end
+//================================================================
+//  OUTPUT
+//================================================================
+always @( posedge clk or negedge reset ) begin
+	if( !reset ) sti_addr <= 0;
+	else if( cur_state == STATE_DUPLICATE && dup_cnt == 0 ) sti_addr <= sti_addr + 1;
 end
 
+wire [3:0] addr_tmp = dup_cnt-4'd1;
 
-//counter
-always@( posedge clk or negedge reset )
-begin
-	if( !reset ) counter <= 4'd15;
-	else if( nx_state == WR_INIT ) counter <= counter - 4'd1;
-	else if( state == BEFORE_RD_FOR || state == FORWARD_FN ) counter <= 4'd0;
-	else if( nx_state == FORWARD || nx_state == BACKWARD ) counter <= counter + 4'd1;
-	else if( nx_state == RD_FOR || nx_state == RD_BACK ) counter <= 4'd0;
-end
-
-//minimum
-always@( posedge clk or negedge reset )
-begin
-	if( !reset ) minimum <= 8'd0;
-	else if( state == FORWARD  ) 
-	begin 
-		if( counter == 4'd1 ) minimum <= res_di;
-		else if( minimum >= res_di ) minimum <= res_di; 
-	end
-	else if( state == BACKWARD )
-	begin
-		if( counter == 4'd1) minimum <= res_di;
-		else if( minimum > (res_di + 8'd1) ) minimum <= res_di + 8'd1;		
-	end
-end
-
-//Control Signal
-always@(*)
-begin
-	case(state)
-	IDLE, BEFORE_RD_FOR, FORWARD_FN, FN:
-	begin
-		sti_rd = 1'd0;
-		res_wr = 1'd0;
-		res_rd = 1'd0;
-	end
-	RD_INIT:
-	begin
-		sti_rd = 1'd1;
-		res_wr = 1'd0;
-		res_rd = 1'd0;
-	end
-	WR_INIT, WR_FOR, WR_BACK:
-	begin
-		sti_rd = 1'd0;
-		res_wr = 1'd1;
-		res_rd = 1'd0;
-	end
-	RD_FOR, FORWARD, RD_BACK, BACKWARD: 
-	begin
-		sti_rd = 1'd0;
-		res_wr = 1'd0;
-		res_rd = 1'd1;	
-	end
-	default: 
-	begin	
-		sti_rd = 1'd0;
-		res_wr = 1'd0;
-		res_rd = 1'd0;	
-	end
+always @( * ) begin
+	case( cur_state )
+		STATE_DUPLICATE:  res_do = idata_buf[addr_tmp];
+		STATE_OUTPUT: begin
+			if( flag_fb ) begin
+					if( res_input_buf > mini+1 ) res_do = mini + 1;
+					else res_do = res_input_buf;
+			end
+			else	res_do = mini + 1;
+		end
+		default: res_do = 0;
 	endcase
 end
 
-//fw_finish
-always@(posedge clk or negedge reset)
-begin
-	if( !reset ) fw_finish <= 1'd0;
-	else if( state == FORWARD_FN ) fw_finish <= 1'd1;
+always @(posedge clk or negedge reset ) begin
+	if( !reset ) dup_cnt <= 4'd0;
+	else if( nx_state == STATE_DUPLICATE ) dup_cnt <= dup_cnt + 1;
+	else dup_cnt <= 0; 
 end
 
-//done
-always@( posedge clk or negedge reset )
-begin
+reg [13:0] addr_forw[3:0];
+
+always @( * ) begin
+	if( !flag_fb ) begin
+		addr_forw[0] = {y-7'd1, x-7'd1} ;
+		addr_forw[1] = {y-7'd1, x     } ;
+		addr_forw[2] = {y-7'd1, x+7'd1} ;
+		addr_forw[3] = {y     , x-7'd1} ;
+	end
+	else begin
+		addr_forw[0] = {y     , x+7'd1} ;
+		addr_forw[1] = {y+7'd1, x-7'd1} ;
+		addr_forw[2] = {y+7'd1, x     } ;
+		addr_forw[3] = {y+7'd1, x+7'd1} ;
+	end
+end	
+
+
+always @(posedge clk or negedge reset ) begin
+	if( !reset ) 							res_addr <= 0;
+	else if( nx_state == STATE_DUPLICATE ) 	res_addr <= {sti_addr,dup_cnt};
+	else if( nx_state == STATE_READ_MEM ) 	res_addr <= {y,x};
+	else if( nx_state == STATE_CALC ) 		res_addr <= ( flag_fb )? addr_forw[cnt_calc] + 1:addr_forw[cnt_calc] - 1;
+	else if( nx_state == STATE_OUTPUT ) 	res_addr <= ( flag_fb )? {y,x} + 1:{y,x} - 1;
+	else if( nx_state == STATE_DELAY ) 		res_addr <= 16383;
+end
+
+always @( posedge clk or negedge reset ) begin
 	if( !reset ) done <= 0;
-	else if( state == FN ) done <= 1;
+	else if( cur_state == STATE_FINISH ) done <= 1;
 end
 
+assign res_wr = ( cur_state == STATE_DUPLICATE || cur_state == STATE_OUTPUT );
+assign res_rd = ( cur_state == STATE_READ_MEM  || cur_state == STATE_CALC   );
 
+assign fw_finish = flag_fb;
+//================================================================
+//  x && y
+//================================================================
+always @(posedge clk or negedge reset) begin
+	if(!reset) begin
+		x <= 7'd0; y <= 7'd0;
+	end
+	else if( nx_state == STATE_READ_MEM ) begin
+		if( flag_fb ) begin
+			x <= x - 1; y<= (x == 0)? y-1:y;
+		end
+		else begin
+			x <= x + 1; y <= ( x == 127 )? y+1 : y; 
+		end
+	end
+	else if( nx_state == STATE_DELAY ) begin
+		x <= 7'd127; y <= 7'd127;
+	end 
+end
+
+always @( posedge clk or negedge reset ) begin
+	if(!reset) cnt_calc <= 0;
+	else if( nx_state == STATE_CALC ) cnt_calc <= cnt_calc + 1; 
+	else cnt_calc <= 0;
+end
+
+//================================================================
+//  flag
+//================================================================
+always @(posedge clk or negedge reset ) begin
+	if(!reset) flag_fb <= 0;
+	else if( cur_state == STATE_READ_MEM ) begin
+		if( res_addr == 16383 ) flag_fb <= 1;
+	end
+end
+
+//================================================================
+//  mini
+//================================================================
+always @(posedge clk or negedge reset ) begin
+	if(!reset) mini <= 0;
+	else if( cur_state == STATE_CALC ) begin
+		if( cnt_calc == 1 ) mini <= res_di;
+		else begin
+			if( res_di < mini ) mini <= res_di;
+		end
+	end
+end
+
+always @(posedge clk or negedge reset ) begin
+	if(!reset) done <= 0;
+	else done <= (cur_state == STATE_FINISH );
+end
+ 
 endmodule
