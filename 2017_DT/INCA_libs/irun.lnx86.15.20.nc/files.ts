79dc873f23cb751c5b0d177e7d9bdce18098ec24
lucynth <98674845+derek8955@users.noreply.github.com>
2023-03-11 09:32:54 +0800

    Delete 2017_DT/INCA_libs directory

--- 2017_DT/INCA_libs/irun.lnx86.15.20.nc/files.ts
+++
@@ -1,4 +0,0 @@
-1487903282 /home/t107360223/Desktop/ic_contest/2017_DT/tsmc13_neg.v
-1645082130 /home/t107360223/Desktop/ic_contest/2017_DT/testfixture.v
-1645081960 /home/t107360223/Desktop/ic_contest/2017_DT/DT.v
-1645082031 /home/t107360223/Desktop/ic_contest/2017_DT/DT_syn.v
