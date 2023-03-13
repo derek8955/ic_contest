94810adfa4af4d18342444f4a24a3232e37620ed
lucynth <98674845+derek8955@users.noreply.github.com>
2023-03-13 21:47:48 +0800

    Delete 2018_univ_IDC/INCA_libs directory

--- 2018_univ_IDC/INCA_libs/irun.lnx86.15.20.nc/files.ts
+++
@@ -1,4 +0,0 @@
-1644481728 /home/t107360223/Desktop/ic_contest/2018_univ_IDC/tsmc13_neg.v
-1648558799 /home/t107360223/Desktop/ic_contest/2018_univ_IDC/LCD_CTRL.v
-1644459864 /home/t107360223/Desktop/ic_contest/2018_univ_IDC/testfixture.v
-1644481301 /home/t107360223/Desktop/ic_contest/2018_univ_IDC/LCD_CTRL_syn.v
