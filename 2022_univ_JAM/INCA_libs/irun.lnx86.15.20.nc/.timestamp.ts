00372eb8e056aa517830fba212503ee01da321c2
lucynth <98674845+derek8955@users.noreply.github.com>
2023-03-16 15:17:26 +0800

    Delete 2022_univ_JAM directory

--- 2022_univ_JAM/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
+++
@@ -1,6 +0,0 @@
-1648616850 /home/t107360223/Desktop/icc/JAM_syn.v
-1648611910 /home/t107360223/Desktop/icc/tsmc13_neg.v
-1648649535 /home/t107360223/Desktop/icc/JAM.v
-1648617227 /home/t107360223/Desktop/icc/tb.sv
-1648735412 /home/t107360223/Desktop/ic_contest/2022_univ_JAM/JAM.v
-1648617227 /home/t107360223/Desktop/ic_contest/2022_univ_JAM/tb.sv
--- 2022_univ_JAM/INCA_libs/irun.lnx86.15.20.nc/files.ts
+++
@@ -1,2 +0,0 @@
-1648617227 /home/t107360223/Desktop/ic_contest/2022_univ_JAM/tb.sv
-1648735412 /home/t107360223/Desktop/ic_contest/2022_univ_JAM/JAM.v
